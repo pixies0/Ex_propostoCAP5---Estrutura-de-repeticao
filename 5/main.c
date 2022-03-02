228ce760034d7ceaa2e5ffc500408f686025dcd0
João Pedro <jpcunha5@gmail.com>
2022-03-02 16:24:28 -0300

    going to 6°

---
+++ 5/main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+  int i, j;
+
+  for (i = 1; i <= 10; i++)
+  {
+    for (j = 0; j <= 10; j++)
+    {
+      printf("%d x %d = %d\n", i, j, i * j);
+    }
+    puts("");
+  }
+
+  return 0;
+}
