42809ff155f3af4e9127bb40653d01d72305ea30
João Pedro <jpcunha5@gmail.com>
2022-03-04 20:37:05 -0300

    *going to 11°*

---
+++ 10/main.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+  int num, somaPar = 0, somaPrimo = 0, i;
+  int j, flag, res;
+
+  for (i = 0; i < 10; i++)
+  {
+    flag = 0;
+    printf("Numero:");
+    scanf("%d", &num);
+    flag = 0;
+
+    if (num % 2 == 0)
+      somaPar += num;
+
+    for (j = 1; j <= num; j++)
+    {
+      res = num % j;
+      if (res == 0)
+        flag++;
+    }
+
+    if (flag == 2)
+      somaPrimo += num;
+  }
+  printf("Soma dos primos:%d\n", somaPrimo);
+  printf("Soma dos pares:%d\n", somaPar);
+}
